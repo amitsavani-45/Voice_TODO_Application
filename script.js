dc32758dd9f4a8c757e8481b4bbe412418d238ce
amitsavani-45 <apsavani4518@gmail.com>
2026-02-11 13:01:02 +0530

    voice_TODO

--- script.js
+++ script.js
@@ -2,8 +2,35 @@ const taskloist=[]
 const listElement=document.getElementById("tasklist");
 const status=document.getElementById("status");
 
-//speech Recognition
+//setup speech Recognition
 
 const speechRecognition=window.speechRecognition;
 const recognition=new speechRecognition();
 recognition.continuous=false;
+recognititon.lang='en-US';
+
+recognition.onresult=(event) =>{
+    const transcript=event.results[0][0].transcript.toLowerCase();
+    statusText.innerText=`Heard: "${transcript}"`
+    if(transcript.startWith("naya task")){
+        const tasktext=transcript.replace()("nayay task","").trim();
+        if(tasktext)
+             addTask(taskText);
+    }
+    else if(transcript.startsWith("deletr task")){
+        const num=parsenInr(transcript.split("")[2])-1;
+        if(!isNaN(num))
+        deleteTask(num);
+}
+else if(transcript.startWith("mark task")){
+       const num=parsenInr(transcript.split("")[2])-1;
+       if(!isNaN(num))
+       markTaskDone(num);
+}
+}
+function addTask(task){
+
+}
+function deleteTask(num){
+    
+}
